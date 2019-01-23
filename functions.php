866db4c4b701505145f1b5e9c5b258a6aece8975
vicLobins89 <vic.lobins@gmail.com>
2019-01-23 11:49:00 +0000

    23 Jan

--- functions.php
+++ functions.php
@@ -371,58 +371,6 @@ function wdm_send_order_to_ext( $order_id ){
 			print_r($item);
 		}
 	}
-	
-	// Open temp file pointer
-    if (!$fp = fopen('php://temp', 'w+')) return FALSE;
-    
-    fputcsv($fp, $data);
-    
-    // Loop data and write to file pointer
-//    while ($line = mysql_fetch_assoc($data)) fputcsv($fp, $line);
-    
-    // Place stream pointer at beginning
-    rewind($fp);
-
-    // Return the data
-    return stream_get_contents($fp);
 }
-
-function send_csv_mail($csvData, $body, $to = 'vic@honey.co.uk', $subject = 'Website Report', $from = 'noreply@chesneys-test-uk.tk') {
-
-    // This will provide plenty adequate entropy
-    $multipartSep = '-----'.md5(time()).'-----';
-
-    // Arrays are much more readable
-    $headers = array(
-        "From: $from",
-        "Reply-To: $from",
-        "Content-Type: multipart/mixed; boundary=".$multipartSep.""
-    );
-
-    // Make the attachment
-    $attachment = chunk_split(base64_encode(wdm_send_order_to_ext($csvData)));
-
-    // Make the body of the message
-    $body = "--$multipartSep\r\n"
-        . "Content-Type: text/plain; charset=ISO-8859-1; format=flowed\r\n"
-        . "Content-Transfer-Encoding: 7bit\r\n"
-        . "\r\n"
-        . "$body\r\n"
-        . "--$multipartSep\r\n"
-        . "Content-Type: text/csv\r\n"
-        . "Content-Transfer-Encoding: base64\r\n"
-        . "Content-Disposition: attachment; filename=\"".$subject."\"\r\n\r\n"
-        . "\r\n"
-        . "$attachment\r\n"
-        . "--$multipartSep--";
-
-    // Send the email, return the result
-    return @mail($to, $subject, $body, implode("\r\n", $headers)); 
-
-}
-
-$array = array(array(1,2,3,4,5,6,7), array(1,2,3,4,5,6,7), array(1,2,3,4,5,6,7));
-
-send_csv_mail($array, "Website Report \r\n \r\n www.carlofontanos.com");
 
 /* DON'T DELETE THIS CLOSING TAG */ ?>
