27a03602a8c06873e15f8e569f4a116695bfde65
vicLobins89 <vic.lobins@gmail.com>
2019-01-23 12:01:49 +0000

    23 Jan

--- functions.php
+++ functions.php
@@ -370,7 +370,50 @@ function wdm_send_order_to_ext( $order_id ){
 		foreach( $items as $key => $item){
 			print_r($item);
 		}
+		
+		$csvAttachment = create_csv_string($data);
+		send_csv_mail($csvAttachment);
 	}
+}
+
+function create_csv_string($data2) {
+  // Open temp file pointer
+  if (!$fp = fopen('php://temp', 'w+')) return FALSE;
+  // Loop data and write to file pointer
+  foreach ($data2 as $line) fputcsv($fp, $line);
+  // Place stream pointer at beginning
+  rewind($fp);
+  // Return the data
+  return stream_get_contents($fp);
+}
+
+function send_csv_mail($csvData, $body, $to = 'vic@honey.co.uk',  $from = 'noreply@chesneys-test-uk.tk' ,$subject = 'Test email with attachment' ) {
+  // This will provide plenty adequate entropy
+  $multipartSep = '-----'.md5(time()).'-----';
+  // Arrays are much more readable
+  $headers = array(
+    "From: $from",
+    "Reply-To: $from",
+    "Content-Type: multipart/mixed; boundary=\"$multipartSep\""
+  );
+  // Make the attachment
+ 
+ $attachment = chunk_split(base64_encode(create_csv_string($csvData))); 
+  // Make the body of the message
+  $body = "--$multipartSep\r\n"
+        . "Content-Type: text/plain; charset=ISO-8859-1; format=flowed\r\n"
+        . "Content-Transfer-Encoding: 7bit\r\n"
+        . "\r\n"
+        . "$body\r\n"
+        . "--$multipartSep\r\n"
+        . "Content-Type: text/csv\r\n"
+        . "Content-Transfer-Encoding: base64\r\n"
+        . "Content-Disposition: attachment; filename=\"AFILE.csv\"\r\n"
+        . "\r\n"
+        . "$attachment\r\n"
+        . "--$multipartSep--";
+   // Send the email, return the result
+   return @mail($to, $subject, $body, implode("\r\n", $headers)); 
 }
 
 /* DON'T DELETE THIS CLOSING TAG */ ?>
