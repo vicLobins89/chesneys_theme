4d1e07863ef6381f36fdc626f00e4106ec0a493d
vicLobins89 <vic.lobins@gmail.com>
2019-01-31 12:40:04 +0000

    31 Jan

--- woocommerce/checkout-functions.php
+++ woocommerce/checkout-functions.php
@@ -100,7 +100,15 @@ function wdm_send_order_to_ext( $order_id ){
 	}
 
 	// Iterating through order shipping items
-	print_r($order->get_items( 'shipping' ));
+	foreach( $order->get_items( 'shipping' ) as $item_id => $shipping_item_obj ){
+		$order_item_name = $shipping_item_obj->get_name();
+		$order_item_type = $shipping_item_obj->get_type();
+		$shipping_method_title = $shipping_item_obj->get_method_title();
+		$shipping_method_total = $shipping_item_obj->get_total();
+		
+		print_r($item_id);
+		print_r($shipping_item_obj);
+	}
 	
 //	print_r($order);
 //	print_r($shipping_type);
