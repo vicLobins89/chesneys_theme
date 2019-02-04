eb0d4ae88efb66caa6e79ff6b5e08eb45e9760bc
vicLobins89 <vic.lobins@gmail.com>
2019-02-04 12:26:40 +0000

    04 Feb

--- single-content_block.php
+++ single-content_block.php
@@ -42,11 +42,6 @@
 									?>
 								</section> <!-- end article section -->
 
-								<footer class="article-footer">
-									<p class="tags"><?php echo get_the_term_list( get_the_ID(), 'custom_tag', '<span class="tags-title">' . __( 'Custom Tags:', 'bonestheme' ) . '</span> ', ', ' ) ?></p>
-
-								</footer>
-
 								<?php comments_template(); ?>
 
 							</article>
