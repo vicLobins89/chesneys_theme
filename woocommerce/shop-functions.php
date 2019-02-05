82bf06f6ae36cd51f8e92a58dca7a341d8fbd3c0
vicLobins89 <vic.lobins@gmail.com>
2019-02-05 15:14:11 +0000

    05 Feb

--- woocommerce/shop-functions.php
+++ woocommerce/shop-functions.php
@@ -4,7 +4,7 @@
 add_action( 'woocommerce_after_shop_loop_item', 'remove_add_to_cart_buttons', 1 );
 
 function remove_add_to_cart_buttons() {
-	if( is_product_category( array('fireplaces', 'stoves') ) ) {
+	if( is_product_category( array('fireplaces', 'stoves') ) || is_shop() ) {
 		remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
 	}
 }
