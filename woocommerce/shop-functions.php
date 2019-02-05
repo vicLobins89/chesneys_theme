37eb13d66dc7a5fb8de5b8645a3acbde3cea194d
vicLobins89 <vic.lobins@gmail.com>
2019-02-05 15:05:02 +0000

    05 Feb

--- woocommerce/shop-functions.php
+++ woocommerce/shop-functions.php
@@ -4,8 +4,22 @@
 add_action( 'woocommerce_after_shop_loop_item', 'remove_add_to_cart_buttons', 1 );
 
 function remove_add_to_cart_buttons() {
-	if( is_product_category( array('fireplaces', 'stoves', 'victorian') ) ) {
+	if( is_product_category( array('fireplaces', 'stoves') ) ) {
 		remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
+	}
+}
+
+add_action('wp','disable_cars_initially');  
+
+function disable_cars_initially() {
+	global $product;
+
+	// Set HERE your category ID, slug or name (or an array)
+	$category = 'fireplaces';
+
+	//Remove Add to Cart button from product description of product with any cat    
+	if ( has_term( $category, 'product_cat', $product->id ) ) {
+		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 	}
 }
 
