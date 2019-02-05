7d9866323191928b271b40990593bc57f21a8630
vicLobins89 <vic.lobins@gmail.com>
2019-02-05 15:08:40 +0000

    05 Feb

--- woocommerce/shop-functions.php
+++ woocommerce/shop-functions.php
@@ -1,7 +1,7 @@
 <?php
 
-// Remove 
-add_action( 'woocommerce_after_shop_loop_item', 'remove_add_to_cart_buttons', 1 );
+// Remove cart button
+//add_action( 'woocommerce_after_shop_loop_item', 'remove_add_to_cart_buttons', 1 );
 
 function remove_add_to_cart_buttons() {
 	if( is_product_category( array('fireplaces', 'stoves') ) ) {
@@ -9,18 +9,18 @@ function remove_add_to_cart_buttons() {
 	}
 }
 
-add_action('wp','disable_cars_initially');  
+add_action( 'wp', 'remove_addcart' );
 
-function disable_cars_initially() {
-	global $product;
-
-	// Set HERE your category ID, slug or name (or an array)
-	$category = array('fireplaces', 'stoves');
-
-	//Remove Add to Cart button from product description of product with any cat    
-	if ( has_term( $category, 'product_cat', $product->id ) ) {
-		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-	}
+function remove_addcart() {
+   $product = get_product();
+   if ( has_term ( array('fireplaces', 'stoves'), 'product_cat') ) {
+       remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
+       remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+       remove_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 30 );
+       remove_action( 'woocommerce_grouped_add_to_cart', 'woocommerce_grouped_add_to_cart', 30 );
+       remove_action( 'woocommerce_variable_add_to_cart', 'woocommerce_variable_add_to_cart', 30 );
+       remove_action( 'woocommerce_external_add_to_cart', 'woocommerce_external_add_to_cart', 30 );
+   }
 }
 
 ?>
