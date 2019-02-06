d2de56b317ae788cf93021d38dc991879dfc82e2
vicLobins89 <vic.lobins@gmail.com>
2019-02-06 14:42:33 +0000

    06 Jan

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -86,6 +86,13 @@ if ( woocommerce_product_loop() ) {
 	
 	$term = get_queried_object();
 	$rows = get_field('rows', $term);
+	
+	$parentcats = get_ancestors($term, 'product_cat');
+
+	foreach($parentcat as $parentcats){
+		echo $parentcat;
+	}
+	
 	echo $term->name;
 	if($rows) {
 		foreach($rows as $row) {
