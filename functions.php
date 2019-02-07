be8111ad20865c442183ef312c2602eef8b13e4c
vicLobins89 <vic.lobins@gmail.com>
2019-02-07 17:07:58 +0000

    07 Feb

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
 
 // LOAD CORE (do not remove)
 require_once( 'library/rarehoney.php' );
