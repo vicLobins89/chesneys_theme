754a4d060d73f2fd1c9958f8621ff209af0e87f1
vicLobins89 <vic.lobins@gmail.com>
2019-02-13 17:41:14 +0000

    13 Feb

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -31,7 +31,7 @@ jQuery(document).ready(function($) {
 		$(this).addClass('active');
 	});
 	
-	$('#content, #inner-header, .socket, .logo').hover(function(){
+	$('#content, #inner-header, .socket, .logo, .primary-nav > li:not(.menu-item-has-children)').hover(function(){
 		$('.primary-nav > li').removeClass('active');
 	});
 	
