e8696d795bc9fef102ecc3d0af869fc674555997
vicLobins89 <vic.lobins@gmail.com>
2019-02-13 16:12:15 +0000

    13 Feb

---
+++ sidebar-nav_widget.php
@@ -0,0 +1,9 @@
+<div id="nav_widget" class="cf" role="complementary">
+
+	<?php if ( is_active_sidebar( 'nav_widget' ) ) : ?>
+
+		<?php dynamic_sidebar( 'nav_widget' ); ?>
+
+	<?php endif; ?>
+
+</div>
