a72f0c558dd6c24f0910f07d602de16cbaf8fa95
vicLobins89 <vic.lobins@gmail.com>
2019-02-19 10:59:04 +0000

    19 Feb

--- classes/acf.php
+++ classes/acf.php
@@ -93,7 +93,7 @@ class CustomACF {
 	
 	public function render_portfolio($portfolio_feed = null) {
 		$all_cats = ($portfolio_feed['all_categories']) ? $portfolio_feed['all_categories'] : get_sub_field('all_categories');
-		$folio_category = ($portfolio_feed['choose_portfolio']) ? $portfolio_feed['choose_portfolio'] : et_sub_field('choose_portfolio');
+		$folio_category = ($portfolio_feed['choose_portfolio']) ? $portfolio_feed['choose_portfolio'] : get_sub_field('choose_portfolio');
 		$post_num = ($portfolio_feed['post_count']) ? $portfolio_feed['post_count'] : get_sub_field('post_count');
 		
 		global $post;
