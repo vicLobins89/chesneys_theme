65d3b133291fceb979249b3b884b12ce4128ba50
vicLobins89 <vic.lobins@gmail.com>
2019-02-19 13:01:08 +0000

    19 Feb

---
+++ sidebar-product_info.php
@@ -0,0 +1,9 @@
+<div id="product_info" class="cf" role="complementary">
+
+	<?php if ( is_active_sidebar( 'product_info' ) ) : ?>
+
+		<?php dynamic_sidebar( 'product_info' ); ?>
+
+	<?php endif; ?>
+
+</div>
