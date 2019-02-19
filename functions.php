b13f8e4bf9bce0331a5e5bed89074f1a20a9c11f
vicLobins89 <vic.lobins@gmail.com>
2019-02-19 15:58:20 +0000

    19 Feb

--- functions.php
+++ functions.php
@@ -297,18 +297,4 @@ function excerpt_in_product_archives() {
 }
 add_action( 'woocommerce_after_shop_loop_item_title', 'excerpt_in_product_archives', 40 );
 
-// Short desc limit
-function prefix_filter_woocommerce_short_description( $post_post_excerpt ) { 
-    // make filter magic happen here... 
-    if(! is_product() ) { // add in conditionals
-        $text = $post_post_excerpt; 
-        $words = 10; // change word length
-        $more = ' […]'; // add a more cta
-        
-        $post_post_excerpt = wp_trim_words( $text, $words, $more );
-    }
-    return $post_post_excerpt; 
-};
-add_filter( 'woocommerce_short_description', 'prefix_filter_woocommerce_short_description' );
-
 /* DON'T DELETE THIS CLOSING TAG */ ?>
