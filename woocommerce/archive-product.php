d2dc7529b6b122f46f4172aceb0cfbd6ea7c0fae
vicLobins89 <vic.lobins@gmail.com>
2019-02-19 15:35:28 +0000

    19 Feb

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -122,7 +122,9 @@ if ( woocommerce_product_loop() ) {
 	
 	// Related products
 	$parent_cats = get_ancestors($term->term_id, 'product_cat');
-	print_r($parent_cats);
+	$category = get_term_by('id', $parent_cats[0] 'product_cat');
+	print_r($category);
+	
 	if( get_term_meta($term->term_id, 'display_type', true) == 'products' ||  get_term_meta($term->term_id, 'display_type', true) == null) {
 		echo do_shortcode('[products category="'.$parent_cats[0]->slug.'" limit="3" columns="3" class="related-products"]');
 	}
