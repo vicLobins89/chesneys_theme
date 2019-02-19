679f11e74173b737ee9e4193c3e0a48a6b388942
vicLobins89 <vic.lobins@gmail.com>
2019-02-19 14:06:22 +0000

    19 Feb

--- library/rarehoney.php
+++ library/rarehoney.php
@@ -278,6 +278,9 @@ function bones_excerpt_more($more) {
 	return '...  <a class="excerpt-read-more" href="'. get_permalink( $post->ID ) . '" title="'. __( 'Read ', 'bonestheme' ) . esc_attr( get_the_title( $post->ID ) ).'">'. __( 'Read more &raquo;', 'bonestheme' ) .'</a>';
 }
 
-
+function excerptLength($length) {
+    return 40;
+}
+add_filter('excerpt_length', 'excerptLength');
 
 ?>
