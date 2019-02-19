88b9dc2248009028ce200f9393f09e9fbe83730b
vicLobins89 <vic.lobins@gmail.com>
2019-02-19 11:20:13 +0000

    19 Feb

--- classes/acf.php
+++ classes/acf.php
@@ -139,7 +139,7 @@ class CustomACF {
 							echo '</p>';
 						}
 						?>
-						<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 						<?php ($row == 1) ? the_excerpt() : '' ; ?>
 					</div>
 				</article>
