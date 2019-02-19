9f307fd5c1c938ecf45c189365181b7e70e7045c
vicLobins89 <vic.lobins@gmail.com>
2019-02-19 16:19:05 +0000

    19 Feb

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -121,9 +121,9 @@ if ( woocommerce_product_loop() ) {
 	}
 	
 	// Related products
-	$parent_cats = get_ancestors($term->term_id, 'product_cat');
-	$category = get_term_by('id', $parent_cats[0], 'product_cat');
 	if( get_term_meta($term->term_id, 'display_type', true) == 'products' ||  get_term_meta($term->term_id, 'display_type', true) == null) {
+		$parent_cats = get_ancestors($term->term_id, 'product_cat');
+		$category = get_term_by('id', $parent_cats[0], 'product_cat');
 		echo '<section class="row entry-content cf related-products"><div class="cf"><div class="col-12">';
 		echo '<h2>You might also like</h2><p>Ariptimus fue et vitioca tquerra vivis, nem merissent grate, ceni sintere mo</p>';
 		echo do_shortcode('[products orderby="rand" category="'.$category->slug.'" limit="3" columns="3" class="related-products"]');
