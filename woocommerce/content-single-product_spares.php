1ecd46769c1ec0d70b4138930d54512025f1af11
vicLobins89 <vic.lobins@gmail.com>
2019-02-20 14:03:41 +0000

    20 Feb

--- woocommerce/content-single-product_spares.php
+++ woocommerce/content-single-product_spares.php
@@ -29,7 +29,7 @@ if ( post_password_required() ) {
 	return;
 }
 ?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class(); ?>>
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class('cf'); ?>>
 
 	<?php
 		/**
