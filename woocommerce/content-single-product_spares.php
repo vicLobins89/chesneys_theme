80f709257aa403f86936758a01f388ebfb03b3b5
vicLobins89 <vic.lobins@gmail.com>
2019-02-20 14:26:22 +0000

    20 Feb

--- woocommerce/content-single-product_spares.php
+++ woocommerce/content-single-product_spares.php
@@ -51,7 +51,7 @@ $attachment_ids = $product->get_gallery_image_ids();
 		if ( $attachment_ids && $product->get_image_id() ) {
 			foreach ( $attachment_ids as $attachment_id ) {
 				//echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_id ), $attachment_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
-				echo wc_get_gallery_image_html( $attachment_id );
+				print_r(wc_get_gallery_image_html( $attachment_id ));
 			}
 		}
 		
