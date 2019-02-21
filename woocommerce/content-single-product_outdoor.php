748821d0a85230f1a9143d89dd74d45f9a511561
vicLobins89 <vic.lobins@gmail.com>
2019-02-21 16:59:33 +0000

    21 Feb

--- woocommerce/content-single-product_outdoor.php
+++ woocommerce/content-single-product_outdoor.php
@@ -73,8 +73,15 @@ if ( post_password_required() ) {
 		 * @hooked woocommerce_upsell_display - 15
 		 * @hooked woocommerce_output_related_products - 20
 		 */
-		//do_action( 'woocommerce_after_single_product_summary' );
+		remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
+		remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+		do_action( 'woocommerce_after_single_product_summary' );
 	?>
 </div>
 
 <?php do_action( 'woocommerce_after_single_product' ); ?>
+
+<?php // Need help module
+$ctas_module = get_post(986);
+$acfClass->render_modules($ctas_module);
+?>
