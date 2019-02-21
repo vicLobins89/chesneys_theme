f1fc85f9a182e3d732ca21242b64caac83a4ce49
vicLobins89 <vic.lobins@gmail.com>
2019-02-21 11:57:34 +0000

    21 Feb

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -50,40 +50,6 @@ jQuery(document).ready(function($) {
 		$('#nav_widget').removeClass('active');
 	});
 	
-	// Quantity button
-	$('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
-
-    $('.quantity').each(function() {
-		var spinner = $(this),
-			input = spinner.find('input[type="number"]'),
-			btnUp = spinner.find('.quantity-up'),
-			btnDown = spinner.find('.quantity-down'),
-			min = input.attr('min'),
-			max = input.attr('max');
-
-			btnUp.click(function() {
-				var oldValue = parseFloat(input.val());
-				if (oldValue >= max) {
-					var newVal = oldValue;
-				} else {
-					var newVal = oldValue + 1;
-				}
-				spinner.find("input").val(newVal);
-				spinner.find("input").trigger("change");
-			});
-
-			btnDown.click(function() {
-				var oldValue = parseFloat(input.val());
-				if (oldValue <= min) {
-					var newVal = oldValue;
-				} else {
-					var newVal = oldValue - 1;
-				}
-				spinner.find("input").val(newVal);
-				spinner.find("input").trigger("change");
-			});
-    });
-	
 	function menuResize(){
 		var menuWidth = $('.primary-nav').outerWidth(),
 			menuHeight = $('.primary-nav').outerHeight(),
