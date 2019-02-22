6367d4ba4dd3ab2796c626e3997a50ae25aad1a8
vicLobins89 <vic.lobins@gmail.com>
2019-02-22 12:02:29 +0000

    22 Feb

--- woocommerce/content-single-product_outdoor.php
+++ woocommerce/content-single-product_outdoor.php
@@ -20,6 +20,12 @@ global $product;
 require_once(__DIR__.'/../classes/acf.php');
 $acfClass = new CustomACF();
 
+add_filter('woocommerce_single_product_image_thumbnail_html','wc_remove_link_on_thumbnails' );
+ 
+function wc_remove_link_on_thumbnails( $html ) {
+     return strip_tags( $html,'<img>' );
+}
+
 /**
  * Hook: woocommerce_before_single_product.
  *
