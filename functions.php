751c1689e9562f902c858ef5a7a8f1c96e5e2b93
vicLobins89 <vic.lobins@gmail.com>
2019-02-25 17:19:09 +0000

    25 Feb

--- functions.php
+++ functions.php
@@ -307,25 +307,6 @@ function excerpt_in_product_archives() {
 add_action( 'woocommerce_after_shop_loop_item_title', 'excerpt_in_product_archives', 40 );
 
 // Sorting
-add_filter( 'woocommerce_get_catalog_ordering_args', 'custom_woocommerce_get_catalog_ordering_args' );
-
-function custom_woocommerce_get_catalog_ordering_args( $args ) {
-    $orderby_value = isset( $_GET['orderby'] ) ? woocommerce_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
-
-    if ( 'alphabetical' == $orderby_value ) {
-        $args['orderby'] = 'title';
-        $args['order'] = 'DESC';
-    }
-
-    return $args;
-}
-
-add_filter( 'woocommerce_default_catalog_orderby_options', 'custom_woocommerce_catalog_orderby' );
-add_filter( 'woocommerce_catalog_orderby', 'custom_woocommerce_catalog_orderby' );
-
-function custom_woocommerce_catalog_orderby( $sortby ) {
-    $sortby['alphabetical'] = __( 'Alphabetical' );
-    return $sortby;
-}
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
 
 /* DON'T DELETE THIS CLOSING TAG */ ?>
