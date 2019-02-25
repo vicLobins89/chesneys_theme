469139caf918796ae33018ed07e44f8b908d7ebd
vicLobins89 <vic.lobins@gmail.com>
2019-02-25 12:56:56 +0000

    25 Feb

--- functions.php
+++ functions.php
@@ -218,6 +218,15 @@ add_filter( 'body_class', 'add_slug_body_class' );
 // Page Excerpt
 add_post_type_support( 'page', 'excerpt' );
 
+// Is tree function
+function is_tree($pid) {      // $pid = The ID of the page we're looking for pages underneath
+	global $post;         // load details about this page
+	if(is_page()&&($post->post_parent==$pid||is_page($pid))) 
+               return true;   // we're at the page or at a sub page
+	else 
+               return false;  // we're elsewhere
+};
+
 
 // WOOCOMMERCE
 function mytheme_add_woocommerce_support() {
--- page.php
+++ page.php
@@ -13,7 +13,7 @@ $acfClass = new CustomACF();
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 							
 							<?php
-							if( !is_front_page() && !is_page('about-us') ) {
+							if( !is_front_page() && !is_tree(91) ) {
 								$menu_breadcrumb = new Menu_Breadcrumb( 'main-nav' );
 								$menu_breadcrumb->render( ' &sol;&sol; ', '<p class="menu-breadcrumb"><a href="'.home_url().'">Homepage</a> // ', '</p>' );
 							} elseif( !is_front_page() ) {
