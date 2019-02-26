be8056d9c3ed2063bd2abc8588b81ff45a9baa6e
vicLobins89 <vic.lobins@gmail.com>
2019-02-26 19:52:23 +0000

    26 Feb

--- woocommerce/archive-product_outdoor.php
+++ woocommerce/archive-product_outdoor.php
@@ -32,7 +32,7 @@ $term = get_queried_object();
 do_action( 'woocommerce_before_main_content' );
 
 ?>
-<header class="row entry-content cf woocommerce-products-header featured top">
+<header class="row entry-content cf woocommerce-products-header">
 	<div class="cf">
 		<?php
 		do_action( 'woocommerce_archive_description' );
