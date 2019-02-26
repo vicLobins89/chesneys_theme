3c5f0ad4b637215e25ba7149cd617e410050fb05
vicLobins89 <vic.lobins@gmail.com>
2019-02-26 19:15:49 +0000

    26 Feb

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -43,31 +43,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
 
 				<li class="woocommerce-order-overview__order order">
-					<?php _e( 'Order number:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_order_number(); ?></strong>
+					<p class="heading"><?php _e( 'Order number:', 'woocommerce' ); ?></p>
+					<p><strong><?php echo $order->get_order_number(); ?></strong></p>
 				</li>
 
 				<li class="woocommerce-order-overview__date date">
-					<?php _e( 'Date:', 'woocommerce' ); ?>
-					<strong><?php echo wc_format_datetime( $order->get_date_created() ); ?></strong>
+					<p class="heading"><?php _e( 'Date:', 'woocommerce' ); ?></p>
+					<p><strong><?php echo wc_format_datetime( $order->get_date_created() ); ?></strong></p>
 				</li>
 
 				<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
 					<li class="woocommerce-order-overview__email email">
-						<?php _e( 'Email:', 'woocommerce' ); ?>
-						<strong><?php echo $order->get_billing_email(); ?></strong>
+						<p class="heading"><?php _e( 'Email:', 'woocommerce' ); ?></p>
+						<p><strong><?php echo $order->get_billing_email(); ?></strong></p>
 					</li>
 				<?php endif; ?>
 
 				<li class="woocommerce-order-overview__total total">
-					<?php _e( 'Total:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_formatted_order_total(); ?></strong>
+					<p class="heading"><?php _e( 'Total:', 'woocommerce' ); ?></p>
+					<p><strong><?php echo $order->get_formatted_order_total(); ?></strong></p>
 				</li>
 
 				<?php if ( $order->get_payment_method_title() ) : ?>
 					<li class="woocommerce-order-overview__payment-method method">
-						<?php _e( 'Payment method:', 'woocommerce' ); ?>
-						<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
+						<p class="heading"><?php _e( 'Payment method:', 'woocommerce' ); ?></p>
+						<p><strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong></p>
 					</li>
 				<?php endif; ?>
 
