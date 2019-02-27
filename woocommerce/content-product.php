e1c8938e5bf5049d570a384f09104bb4226c7f0a
vicLobins89 <vic.lobins@gmail.com>
2019-02-27 15:32:19 +0000

    27 Feb

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -64,7 +64,6 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	 * @hooked woocommerce_template_loop_rating - 5
 	 * @hooked woocommerce_template_loop_price - 10
 	 */
-	remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10);
 	do_action( 'woocommerce_after_shop_loop_item_title' );
 
 	/**
