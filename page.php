98ad22177bd829b0a4678c29831ab05c440a06dc
vicLobins89 <vic.lobins@gmail.com>
2019-03-01 12:31:35 +0000

    01 Mar

--- page.php
+++ page.php
@@ -13,7 +13,7 @@ $acfClass = new CustomACF();
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 							
 							<?php
-							$no_breadcrumb = array( 'Basket', 'Checkout', 'my-account' );
+							$no_breadcrumb = array( 'Basket', 'Checkout', 'my-account', 'Contact Us' );
 							if( !is_front_page() && !is_tree(91) && !is_tree(93) && !is_page($no_breadcrumb) ) {
 								$menu_breadcrumb = new Menu_Breadcrumb( 'main-nav' );
 								$menu_breadcrumb->render( ' &sol;&sol; ', '<p class="menu-breadcrumb"><a href="'.home_url().'">Homepage</a> // ', '</p>' );
