aaf23a0d683c6e35649cb3b044542f0101b37bec
vicLobins89 <vic.lobins@gmail.com>
2019-03-01 17:02:58 +0000

    01 Mar

--- taxonomy-portfolio_cat.php
+++ taxonomy-portfolio_cat.php
@@ -2,7 +2,6 @@
 get_header();
 require_once('classes/acf.php');
 $acfClass = new CustomACF();
-global $post;
 ?>
 
 			<div id="content">
@@ -29,7 +28,7 @@ global $post;
 							
 							<section class="entry-content row post-content cf"><div class="cf"><div class="col-12">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+							<?php while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf col-4' ); ?> role="article">
 								
@@ -45,7 +44,7 @@ global $post;
 
 									<?php bones_page_navi(); ?>
 
-							<?php else : ?>
+							<?php //else : ?>
 
 									<article id="post-not-found" class="hentry cf">
 											<header class="article-header">
@@ -56,7 +55,7 @@ global $post;
 										</section>
 									</article>
 
-							<?php endif; ?>
+							<?php //endif; ?>
 								
 							</div></div></section>
 							
