c9d468b8596612a836ddde5c931bed71dc495c34
vicLobins89 <vic.lobins@gmail.com>
2019-03-01 16:12:02 +0000

    01 Mar

---
+++ sidebar-folio_header.php
@@ -0,0 +1,9 @@
+<section id="folio_header" class="entry-content row cf"><div class="cf"><div class="col-12">
+
+	<?php if ( is_active_sidebar( 'folio_header' ) ) : ?>
+
+		<?php dynamic_sidebar( 'folio_header' ); ?>
+
+	<?php endif; ?>
+
+</div></div></section>
