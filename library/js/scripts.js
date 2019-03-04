3ab1bc322e28fa9904391ba2188571bc47f373e3
vicLobins89 <vic.lobins@gmail.com>
2019-03-04 18:46:08 +0000

    4 Mar

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -22,15 +22,15 @@ jQuery(document).ready(function($) {
 	viewport = updateViewportDimensions();
 	
 	var value = [];
-	$('.js-brochure-input').on('change', function(){
-		$(this).each(function(i, el) {
+	$('.js-brochure-input').each(function() {
+		$(this).on('change', function(i, el){
 			if( !$(this).is(':checked') ) {
 				value.splice(i, 1);
 			} else {
 				value.push($(this).val());
 			}
 			$('input.brochures').val(value);
-			console.log(i + $(el));
+			console.log(i + el);
 		});
 	});
 	
