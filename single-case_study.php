4abd134f83ea4614daae69d6000dbad11abea033
vicLobins89 <vic.lobins@gmail.com>
2019-03-04 12:39:47 +0000

    4 Mar

--- single-case_study.php
+++ single-case_study.php
@@ -21,27 +21,31 @@ $acfClass = new CustomACF();
 						<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemprop="blogPost" itemtype="http://schema.org/BlogPosting">
 							
 						<?php // MAIN CONTENT ?>
-							<?php if( has_post_thumbnail() ) : ?>
-								<section class="row entry-content cf top featured" itemprop="articleBody">
-									<div class="cf">
-									<?php the_post_thumbnail('full'); ?>
-									
-									<div class="featured-copy">
-										<h1 class="h2 lhs"><?php the_title(); ?></h1>
-									</div>
-									</div>
-								</section>
-							<?php else : ?>
-								<section class="row entry-content cf top" itemprop="articleBody"><div class="cf">
-									<div class="col-12">
-										<h1 class="h2" style="text-align: center"><?php the_title(); ?></h1>
-									</div>
-								</div></section>
-							<?php endif; // MAIN CONTENT ?>
+						<?php if( has_post_thumbnail() ) : ?>
+							<section class="row entry-content cf top featured" itemprop="articleBody">
+								<div class="cf">
+								<?php the_post_thumbnail('full'); ?>
 
-						<section class="entry-content row cf post-content" itemprop="articleBody">
-							<?php the_content(); ?>
-						</section> <?php // end article section ?>
+								<div class="featured-copy">
+									<h1 class="h2 lhs"><?php the_title(); ?></h1>
+								</div>
+								</div>
+							</section>
+						<?php else : ?>
+							<section class="row entry-content cf top" itemprop="articleBody"><div class="cf">
+								<div class="col-12">
+									<h1 class="h2" style="text-align: center"><?php the_title(); ?></h1>
+								</div>
+							</div></section>
+						<?php endif; // MAIN CONTENT ?>
+
+						<?php
+						if( !empty( get_the_content() ) ) {
+							echo '<section class="entry-content row cf post-content" itemprop="articleBody">';
+							the_content(); 
+							echo '</section>';
+						}
+						// end article section ?>
 
 						<?php // ACF FIELDS ?>
 						<?php $acfClass->page_rows(); ?>
