d8f34f2b9d2703c02b7b003433fdabc824a1c020
vicLobins89 <vic.lobins@gmail.com>
2019-03-05 09:28:09 +0000

    05 Mar

--- functions.php
+++ functions.php
@@ -62,7 +62,7 @@ function my_theme_enqueue_styles() {
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
+        get_stylesheet_directory_uri() . '-child/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
