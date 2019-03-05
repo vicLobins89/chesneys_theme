1244270e6ecfd5b2f2a6980054e5ccefe2cf3658
vicLobins89 <vic.lobins@gmail.com>
2019-03-05 09:21:31 +0000

    05 Mar

--- functions.php
+++ functions.php
@@ -56,6 +56,19 @@ function rarehoney_init() {
 add_action( 'after_setup_theme', 'rarehoney_init' );
 
 
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'bones-stylesheet';
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
 /************* OEMBED SIZE OPTIONS *************/
 
 if ( ! isset( $content_width ) ) {
