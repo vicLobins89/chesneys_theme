4fec206e764330ce1e543835d1110793e3585911
vicLobins89 <vic.lobins@gmail.com>
2019-03-05 16:17:27 +0000

    05 Mar

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-			<div id="content" class="push">
+			<div id="content" class="row entry-content wrap push">
 
 				<div id="inner-content" class="cf">
 					
@@ -19,21 +19,8 @@
 								<section class="entry-content">
 									
 									<h3 class="search-title entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-
-									<p class="byline entry-meta vcard">
-											<?php printf( __( 'Posted %1$s', 'bonestheme' ),
-											/* the time the post was published */
-											'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>'
-											); ?>
-									</p>
 									
 									<?php the_excerpt( '<span class="read-more">' . __( 'Read more &raquo;', 'bonestheme' ) . '</span>' ); ?>
-									
-									<?php if(get_the_category_list(', ') != ''): ?>
-                  					<?php printf( __( 'Filed under: %1$s', 'bonestheme' ), get_the_category_list(', ') ); ?>
-                  					<?php endif; ?>
-
-                 					<?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 								</section>
 
 							</article>
