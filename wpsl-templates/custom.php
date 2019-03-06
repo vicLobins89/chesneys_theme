4f3d61189cb0403d917d503090a3d255b465594b
vicLobins89 <vic.lobins@gmail.com>
2019-03-06 16:02:37 +0000

    06 Mar

--- wpsl-templates/custom.php
+++ wpsl-templates/custom.php
@@ -1,5 +1,5 @@
 <?php 
-global $wpsl_settings, $wpsl, $wpdb;
+global $wpsl_settings, $wpsl;
 
 $output         = $this->get_custom_css(); 
 $autoload_class = ( !$wpsl_settings['autoload'] ) ? 'class="wpsl-not-loaded"' : '';
@@ -49,14 +49,7 @@ $output .= "\t" . '</div>' . "\r\n";
     
 $output .= "\t" . '<div id="wpsl-gmap" class="wpsl-gmap-canvas"></div>' . "\r\n";
 
-$output .= "\t" . '<div id="wpsl-result-list">' . "\r\n";
-$output .= "\t\t" . '<div id="wpsl-stores" '. $autoload_class .'>' . "\r\n";
-$output .= "\t\t\t" . '<ul></ul>' . "\r\n";
-$output .= "\t\t" . '</div>' . "\r\n";
-$output .= "\t\t" . '<div id="wpsl-direction-details">' . "\r\n";
-$output .= "\t\t\t" . '<ul></ul>' . "\r\n";
-$output .= "\t\t" . '</div>' . "\r\n";
-$output .= "\t" . '</div>' . "\r\n";
+
 
 if ( $wpsl_settings['show_credits'] ) { 
     $output .= "\t" . '<div class="wpsl-provided-by">'. sprintf( __( "Search provided by %sWP Store Locator%s", "wpsl" ), "<a target='_blank' href='https://wpstorelocator.co'>", "</a>" ) .'</div>' . "\r\n";
@@ -64,5 +57,4 @@ if ( $wpsl_settings['show_credits'] ) {
 
 $output .= '</div>' . "\r\n";
 
-print_r($wpdb->posts);
 return $output;
