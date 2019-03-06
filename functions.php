dbaa37566e584a881fa374590f5c46c7f4d88750
vicLobins89 <vic.lobins@gmail.com>
2019-03-06 16:25:33 +0000

    06 Mar

--- functions.php
+++ functions.php
@@ -268,12 +268,6 @@ add_filter( 'wpsl_templates', 'custom_templates' );
 
 function custom_templates( $templates ) {
 
-    /**
-     * The 'id' is for internal use and must be unique ( since 2.0 ).
-     * The 'name' is used in the template dropdown on the settings page.
-     * The 'path' points to the location of the custom template,
-     * in this case the folder of your active theme.
-     */
     $templates[] = array (
         'id'   => 'custom',
         'name' => 'Custom template',
