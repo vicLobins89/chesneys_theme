2de6e5e618ef4c994afa4935558b042758490bdf
vicLobins89 <vic.lobins@gmail.com>
2019-03-06 15:49:42 +0000

    06 Mar

--- functions.php
+++ functions.php
@@ -263,6 +263,26 @@ if (function_exists('geoip_detect2_get_info_from_current_ip')) {
 	}
 }
 
+//STORE LOCATOR
+add_filter( 'wpsl_templates', 'custom_templates' );
+
+function custom_templates( $templates ) {
+
+    /**
+     * The 'id' is for internal use and must be unique ( since 2.0 ).
+     * The 'name' is used in the template dropdown on the settings page.
+     * The 'path' points to the location of the custom template,
+     * in this case the folder of your active theme.
+     */
+    $templates[] = array (
+        'id'   => 'custom',
+        'name' => 'Custom template',
+        'path' => get_stylesheet_directory() . '/' . 'wpsl-templates/custom.php',
+    );
+
+    return $templates;
+}
+
 //WOOCOMMERCE Functions
 require_once( 'woocommerce/shop-functions.php' );
 require_once( 'woocommerce/checkout-functions.php' );
