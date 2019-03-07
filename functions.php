aeeb4e34a7430d285ad70c104f5f5340be73f4e1
vicLobins89 <vic.lobins@gmail.com>
2019-03-07 16:43:41 +0000

    07 Mar

--- functions.php
+++ functions.php
@@ -284,7 +284,7 @@ if (function_exists('geoip_detect2_get_info_from_current_ip')) {
 
 // No Add to cart
 function remove_add_to_cart_buttons() {
-	if( is_product_category( array( 'firepalces', 'stoves'))) {
+	if( is_product_category( array( 'fireplaces', 'stoves'))) {
 		remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
 		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
