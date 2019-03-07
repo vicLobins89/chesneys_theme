e76e24399ae8b29436effb22e20af240a2762491
vicLobins89 <vic.lobins@gmail.com>
2019-03-07 16:57:51 +0000

    07 Mar

--- functions.php
+++ functions.php
@@ -282,17 +282,6 @@ if (function_exists('geoip_detect2_get_info_from_current_ip')) {
 	}
 }
 
-// No Add to cart
-function remove_add_to_cart_buttons() {
-	if( is_product_category( array( 'fireplaces', 'victorian', 'reproduction', 'stoves'))) {
-		remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
-		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
-		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-		remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
-	}
-}
-add_action( 'woocommerce_after_shop_loop_item', 'remove_add_to_cart_buttons', 1 );
-
 //STORE LOCATOR
 function custom_templates( $templates ) {
 
