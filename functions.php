9f4e8c0164db676df834e7a28634709b6449fdae
vicLobins89 <vic.lobins@gmail.com>
2019-03-08 15:39:28 +0000

    08 Mar

--- functions.php
+++ functions.php
@@ -380,7 +380,7 @@ function excerpt($limit) {
 		$excerpt = implode(" ",$excerpt);
 	}	
 	$excerpt = preg_replace('`[[^]]*]`','',$excerpt);
-	return $excerpt;
+	return substr($excerpt,0,strpos($excerpt,'.')+1);
 }
 
 function excerpt_in_product_archives() {
@@ -388,7 +388,14 @@ function excerpt_in_product_archives() {
 		echo '<p>'.excerpt(20).'</p>';
 	}
 }
-//add_action( 'woocommerce_after_shop_loop_item_title', 'excerpt_in_product_archives', 7 );
+add_action( 'woocommerce_after_shop_loop_item_title', 'excerpt_in_product_archives', 7 );
+
+add_filter(
+	'the_excerpt',
+	function ($excerpt) {
+		return substr($excerpt,0,strpos($excerpt,'.')+1);
+	}
+);
 
 // Sorting
 remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
