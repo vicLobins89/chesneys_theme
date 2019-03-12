bf1a029a1f880771381f144a88a5267d6773ba7c
vicLobins89 <vic.lobins@gmail.com>
2019-03-12 09:46:54 +0000

    12 Mar

--- page.php
+++ page.php
@@ -85,38 +85,6 @@ $acfClass = new CustomACF();
 							<?php endif; ?>
 
 							</article>
-				
-				<?php
-				$orderby = 'name';
-				$order = 'asc';
-				$hide_empty = false ;
-				$cat_args = array(
-					'orderby'    => $orderby,
-					'order'      => $order,
-					'hide_empty' => $hide_empty,
-				);
-
-				$product_categories = get_terms( 'product_cat', $cat_args );
-
-				if( !empty($product_categories) ){
-					echo '
-
-				<ul>';
-					foreach ($product_categories as $key => $category) {
-						echo '
-
-				<li>';
-						echo '<a href="'.get_term_link($category).'" >';
-						echo get_term_link($category);
-						echo '</a>';
-						echo '</li>';
-					}
-					echo '</ul>
-
-
-				';
-				}
-				?>
 
 							<?php endwhile; endif; ?>
 
