2db7a836a2c28fa3888470b392aa07ca5fe533f3
vicLobins89 <vic.lobins@gmail.com>
2019-03-18 09:59:36 +0000

    18 Mar

--- header.php
+++ header.php
@@ -6,16 +6,6 @@
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
 	<head>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-6198567-14"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments);}
-		  gtag('js', new Date());
-
-		  gtag('config', 'UA-6198567-14');
-		</script>
-
 		<meta charset="utf-8">
 
 		<?php // force Internet Explorer to use the latest rendering engine available ?>
