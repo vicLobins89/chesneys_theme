f0246bbe53e14572a44cf1443accb4b12b296881
vicLobins89 <vic.lobins@gmail.com>
2019-03-18 11:19:22 +0000

    18 Mar

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -30,6 +30,19 @@ jQuery(document).ready(function($) {
 		e.preventDefault();
 	});
 	
+	// Callback
+	$('a.callback').click(function(e){
+		e.preventDefault();
+		var productName = $(this).closest('.product').find('h2').text();
+		$('input.product').val(productName);
+		$('#wpcf7-f4091-o1 ').addClass('active');
+	});
+	
+	$('a.close-overlay').click(function(e){
+		e.preventDefault();
+		$('#wpcf7-f4091-o1 ').removeClass('active');
+	});
+	
 	// Brochure Request
 	var pdfHref = [],
 		pdfName = [];
