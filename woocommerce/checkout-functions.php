6e7b659761e109712617b2d30cb46981ccfb810c
vicLobins89 <vic.lobins@gmail.com>
2019-03-18 15:50:44 +0000

    18 Mar

--- woocommerce/checkout-functions.php
+++ woocommerce/checkout-functions.php
@@ -222,7 +222,7 @@ function create_csv($records) {
     return $filepath;
 }
 
-function send_csv_mail($csv_data, $body, $to = 'vic.lobins@gmail.co.uk, vitalijs_l@hotmail.co.uk, vic@honey.co.uk, SwiftcareAdmin@Swiftcareuk.com, adam@chesneys.co.uk, lucy.powell@swiftcareuk.com, rachel@swiftcareuk.com',  $from = 'Chesneys Order <no-reply@chesneys.co.uk>', $subject = 'Product Order from Chesneys.co.uk') {
+function send_csv_mail($csv_data, $body, $to = 'vic@honey.co.uk, SwiftcareAdmin@Swiftcareuk.com, adam@chesneys.co.uk',  $from = 'Chesneys Order <no-reply@chesneys.co.uk>', $subject = 'Product Order from Chesneys.co.uk') {
 	
 	$today = date("d-m-y");
 
