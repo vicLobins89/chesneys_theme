e527a6c668d3fed1713469100a437ee0e3ff9580
vicLobins89 <vic.lobins@gmail.com>
2019-04-01 15:10:51 +0100

    1 Apr

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -136,7 +136,7 @@ jQuery(document).ready(function($) {
 		var zip = new JSZip();
 		var brochures = zip.folder("brochures");
 		
-		for( var i = 0; i < files.length; i++ ) {
+		for( var i = 0; i < data.length; i++ ) {
 			brochures.file(data[i]);
 		}
 
