b5d0628cad41086b1abeef8e5048b004e7e435a2
vicLobins89 <vic.lobins@gmail.com>
2019-04-03 15:10:09 +0100

    3 Apr

--- functions.php
+++ functions.php
@@ -329,12 +329,12 @@ function woo_custom_order_button_text() {
 add_filter( 'woocommerce_order_button_text', 'woo_custom_order_button_text' );
 
 // Prices for trade
-//add_filter('woocommerce_product_get_price', 'custom_price_trade', 10, 2);
+add_filter('woocommerce_product_get_price', 'custom_price_trade', 10, 2);
 function custom_price_trade($price, $product) {
     if (!is_user_logged_in()) return $price;
 
     //check if the product is in a category you want, let say shirts
-    if( has_term( 'stove-spares', 'product_cat' ,$product->ID) ) {
+    if( has_term( 'stove-spares', 'product_cat', $product->get_id()) ) {
         //check if the user has a role of dealer using a helper function, see bellow
         if (has_role_trade('trade')){
             //give user 10% of
@@ -345,7 +345,7 @@ function custom_price_trade($price, $product) {
 }
 function has_role_trade($role = '',$user_id = null){
     if ( is_numeric( $user_id ) )
-        $user = get_user_by( 'id',$user_id );
+        $user = get_user_by( 'id', $user_id );
     else
         $user = wp_get_current_user();
 
