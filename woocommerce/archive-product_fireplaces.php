05516ef897453f88556a1ceb9a9198cf451c8ca8
vicLobins89 <vic.lobins@gmail.com>
2019-04-08 15:30:46 +0100

    Products Push

--- woocommerce/archive-product_fireplaces.php
+++ woocommerce/archive-product_fireplaces.php
@@ -25,8 +25,8 @@ $term = get_queried_object();
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 20);
 remove_action('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10);
-remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
-remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
+//remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+//remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
 
 /**
  * Hook: woocommerce_before_main_content.
