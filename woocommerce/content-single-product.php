d0e665d37ec61a49a431fb86691ecc4b3bd72a58
vicLobins89 <vic.lobins@gmail.com>
2019-04-11 15:17:57 +0100

    11 Apr

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -66,7 +66,6 @@ if ( post_password_required() ) {
 		 */
 		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
-		remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 		add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 25 );
 		
 		echo '<div class="short-desc">';
