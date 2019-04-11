835854894a231b4827d49fcb180daa601715374c
vicLobins89 <vic.lobins@gmail.com>
2019-04-11 11:20:50 +0100

    11 Apr

--- woocommerce/taxonomy-product_cat.php
+++ woocommerce/taxonomy-product_cat.php
@@ -21,7 +21,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $term = get_queried_object();
 
-if( term_is_ancestor_of(16, $term->term_id, 'product_cat') || term_is_ancestor_of(56, $term->term_id, 'product_cat') || is_product_category(16) || is_product_category(56) ) {
+if( 
+	term_is_ancestor_of(16, $term->term_id, 'product_cat') ||
+	is_product_category(16) ||
+	term_is_ancestor_of(56, $term->term_id, 'product_cat') ||
+	is_product_category(56) ||
+	term_is_ancestor_of(713, $term->term_id, 'product_cat') ||
+	is_product_category(713)
+) {
 	// Frireplaces & Stoves
 	wc_get_template( 'archive-product_fireplaces.php' );
 } elseif( term_is_ancestor_of(67, $term->term_id, 'product_cat') || is_product_category(67) || term_is_ancestor_of(68, $term->term_id, 'product_cat') || is_product_category(68) ) {
