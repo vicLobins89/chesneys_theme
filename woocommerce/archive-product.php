49de6bf3f03a7d8337f5cdab286cb22ae84e2c00
vicLobins89 <vic.lobins@gmail.com>
2019-04-17 10:52:14 +0100

    17 Apr

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -59,7 +59,7 @@ if ( woocommerce_product_loop() ) {
 	?>
 	<section class="entry-content row cf shop-loop"><div class="cf">
 	<?php
-	if( $blog_id == 5 && (term_is_ancestor_of(16, $term->term_id, 'product_cat') || is_product_category(16)) ) {
+	if( $blog_id == 5 && (term_is_ancestor_of(16, $term->term_id, 'product_cat') || is_product_category(16) || term_is_ancestor_of(68, $term->term_id, 'product_cat') || is_product_category(68)) ) {
 		get_sidebar('filter');
 		echo '<div class="col-9">';
 	} else {
