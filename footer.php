ea445566034a01f3e22f97671e0866d953d0c6d6
vicLobins89 <vic.lobins@gmail.com>
2019-04-18 10:32:11 +0100

    18 Apr

--- footer.php
+++ footer.php
@@ -6,8 +6,9 @@ $blog_id = get_current_blog_id();
 <?php //echo do_shortcode('[geoip_detect2_show_if country="US"] TEXT [/geoip_detect2_show_if]'); ?>
 
 <?php
+$sidebar = get_sidebar('geo_popup');
 if( $blog_id == 5 ) {
-	echo do_shortcode('[geoip_detect2_show_if country="GB"] TEXT [/geoip_detect2_show_if]');
+	echo do_shortcode('[geoip_detect2_show_if country="GB"] ' . $sidebar . ' [/geoip_detect2_show_if]');
 	echo geoip_detect2_get_current_source_description();
 }
 ?>
