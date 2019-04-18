5b5dd7206efffda777f32280fe9a4d9d784f3b5d
vicLobins89 <vic.lobins@gmail.com>
2019-04-18 10:40:37 +0100

    18 Apr

---
+++ sidebar-geo_popup.php
@@ -0,0 +1,9 @@
+<div id="geo_popup" class="sidebar cf" role="complementary">
+
+	<?php if ( is_active_sidebar( 'geo_popup' ) ) : ?>
+
+		<?php dynamic_sidebar( 'geo_popup' ); ?>
+
+	<?php endif; ?>
+
+</div>
