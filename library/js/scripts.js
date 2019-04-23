d6486e5caf05e30f71ee6f90271e8db6480c47ea
vicLobins89 <vic.lobins@gmail.com>
2019-04-23 15:37:56 +0100

    23 Apr

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -25,6 +25,9 @@ jQuery(document).ready(function($) {
 	
 	viewport = updateViewportDimensions();
 	
+	// Filter classes
+	$('li .woof_childs_list_li:has(ul)').addClass('parent_li');
+	
 	// Geo close button
 	$('#geo_popup .close-geo').click(function(e){
 		e.preventDefault();
