31a39dc22885cb680479cd6755e6b51288e85277
vicLobins89 <vic.lobins@gmail.com>
2019-05-20 16:20:20 +0100

    API

--- woocommerce/checkout-functions.php
+++ woocommerce/checkout-functions.php
@@ -179,8 +179,8 @@ function wdm_send_order_to_ext( $order_id ){
 //		"currency" => $currency,
 //		"customer_id" => $user_id,
 		"customer_note" => $notes,
-//		"date_created" => $order_date,
-//		"date_created_gmt" => $order_date,
+		"date_created" => $order_date,
+		"date_created_gmt" => $order_date,
 		"dealer_code" => $dealer_code,
 //		"discount_total" => $order_discount_total,
 
