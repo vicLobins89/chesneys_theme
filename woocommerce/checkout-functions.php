2994c0288e33a37efaafb637858ce1090b390e14
vicLobins89 <vic.lobins@gmail.com>
2019-05-20 17:00:47 +0100

    API

--- woocommerce/checkout-functions.php
+++ woocommerce/checkout-functions.php
@@ -225,8 +225,8 @@ function wdm_send_order_to_ext( $order_id ){
 }
 
 function send_api_call_ches($data) {
-     //$endpoint = "https://core.chesneys.co.uk/wcf/ChesneysWoocommerceService.svc/Test";
-     $endpoint = "https://en74kwaytzvmt.x.pipedream.net/";
+     $endpoint = "https://core.chesneys.co.uk/wcf/ChesneysWoocommerceService.svc/Test";
+//     $endpoint = "https://en74kwaytzvmt.x.pipedream.net/";
     
      // JSON
      $options = array(
