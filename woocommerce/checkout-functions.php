b584b2f341405f4f64c6bc5b1cc1756b034114b0
vicLobins89 <vic.lobins@gmail.com>
2019-05-20 16:48:06 +0100

    API

--- woocommerce/checkout-functions.php
+++ woocommerce/checkout-functions.php
@@ -18,7 +18,7 @@ function wdm_send_order_to_ext( $order_id ){
 	$shipping_type = $order->get_shipping_method();
 	$notes = $order->get_customer_note();
 	$currency = get_woocommerce_currency();
-//	$user_id = $order->get_user_id();
+	$user_id = $order->get_user_id();
 	$dealer_code = get_post_meta( $order_id, 'Dealer Code', true );
 
 	// set the address fields
@@ -177,7 +177,7 @@ function wdm_send_order_to_ext( $order_id ){
 		"billing_state" => $address['billing_state'],
 		
 		"currency" => $currency,
-//		"customer_id" => $user_id,
+		"customer_id" => $user_id,
 		"customer_note" => $notes,
 		"date_created" => $order_date,
 		"date_created_gmt" => $order_date,
