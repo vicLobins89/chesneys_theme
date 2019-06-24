c525a47aaa19124bef0dca5580d2acb2c5451c7a
vicLobins89 <vic.lobins@gmail.com>
2019-06-24 13:12:21 +0100

    US

--- sidebar-filter.php
+++ sidebar-filter.php
@@ -1,4 +1,4 @@
-<div id="filter" class="sidebar col-3 cf" role="complementary" style="display: none;">
+<div id="filter" class="sidebar col-3 cf" role="complementary">
 
 	<?php if ( is_active_sidebar( 'filter' ) ) : ?>
 
