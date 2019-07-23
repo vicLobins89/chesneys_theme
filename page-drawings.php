c1d8eb2eb015ca8e5e9c60943044527f2ed304d9
vicLobins89 <vic.lobins@gmail.com>
2019-07-23 13:54:29 +0100

    js

---
+++ page-drawings.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ Template Name: Drawings Page
+*/
+
+get_header();
+require_once('classes/acf.php');
+$acfClass = new CustomACF();
+?>
+
+			<div id="content">
+
+				<div id="inner-content" class="cf">
+
+						<div id="main" class="cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+								
+							<?php // MAIN CONTENT ?>
+							<?php
+							$the_content = get_the_content();
+							if( has_post_thumbnail() && is_page() ) :
+							?>
+								<section class="row entry-content cf top featured" itemprop="articleBody">
+									<div class="cf">
+									<?php the_post_thumbnail('full'); ?>
+									
+									<div class="featured-copy">
+										<h1 class="h2 lhs"><?php the_title(); ?></h1>
+										<?php the_content(); ?>
+									</div>
+									</div>
+								</section>
+
+							<?php elseif( !empty($the_content) ) :
+								$layout = get_field('editor_layout');
+								if( $layout === 'hide' ) {
+									echo '<section class="row entry-content cf top" style="display: none;">';
+									echo '<div class="cf">';
+								} else if( $layout === 'wrap' ) {
+									echo '<section class="row entry-content wrap cf top" itemprop="articleBody">';
+									echo '<div class="cf">';
+								} else if( $layout === 'full' ) {
+									echo '<section class="row entry-content full cf top" itemprop="articleBody">';
+									echo '<div class="cf">';
+								} else {
+									echo '<section class="row entry-content cf top" itemprop="articleBody">';
+									echo '<div class="cf">';
+								}
+								?>
+									<div class="col-12">
+										<?php the_content(); ?>
+									</div>
+								</div></section>
+							<?php endif; // MAIN CONTENT ?>
+							
+							<?php // ACF FIELDS ?>
+							<?php $acfClass->page_rows(); ?>
+                    
+                            <ul class="client-list cf">
+                            <?php
+                            $dir = '/wp-content/uploads/sites/5/drawings/';
+                            foreach( glob( $dir.'*', GLOB_ONLYDIR ) as $folder ) {
+                                echo '<li>'.$folder.'</li>';
+                            }
+                            ?>
+                            </ul>
+
+							</article>
+
+							<?php endwhile; endif; ?>
+
+						</div>
+
+				</div>
+
+			</div>
+
+<?php get_footer(); ?>
