990bc741d94178678ced851f853148c5870f5f12
vicLobins89 <vic.lobins@gmail.com>
2019-07-24 15:46:39 +0100

    Sitemap

---
+++ page-sitemap.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ Template Name: Sitemap Page
+*/
+
+get_header();
+require_once('classes/acf.php');
+$acfClass = new CustomACF();
+?>
+
+			<div id="content">
+
+				<div id="inner-content" class="cf">
+
+						<div id="main" class="cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+								
+							<?php // MAIN CONTENT ?>
+							<?php
+							$the_content = get_the_content();
+							if( has_post_thumbnail() && is_page() ) :
+							?>
+								<section class="row entry-content cf top featured" itemprop="articleBody">
+									<div class="cf">
+									<?php the_post_thumbnail('full'); ?>
+									
+									<div class="featured-copy">
+										<h1 class="h2 lhs"><?php the_title(); ?></h1>
+										<?php the_content(); ?>
+									</div>
+									</div>
+								</section>
+
+							<?php elseif( !empty($the_content) ) :
+								$layout = get_field('editor_layout');
+								if( $layout === 'hide' ) {
+									echo '<section class="row entry-content cf top" style="display: none;">';
+									echo '<div class="cf">';
+								} else if( $layout === 'wrap' ) {
+									echo '<section class="row entry-content wrap cf top" itemprop="articleBody">';
+									echo '<div class="cf">';
+								} else if( $layout === 'full' ) {
+									echo '<section class="row entry-content full cf top" itemprop="articleBody">';
+									echo '<div class="cf">';
+								} else {
+									echo '<section class="row entry-content cf top" itemprop="articleBody">';
+									echo '<div class="cf">';
+								}
+								?>
+									<div class="col-12">
+										<?php the_content(); ?>
+									</div>
+								</div></section>
+							<?php endif; // MAIN CONTENT ?>
+							
+							<?php // ACF FIELDS ?>
+							<?php $acfClass->page_rows(); ?>
+                    
+                            <?php
+                            wp_nav_menu(array(
+                                'container' => false,
+                                'container_class' => 'sitemap cf',
+                                'menu' => __( 'The Main Menu', 'bonestheme' ),
+                                'menu_class' => 'sitemap cf',
+                                'theme_location' => 'main-nav'
+                            ));
+                            ?>
+
+							</article>
+
+							<?php endwhile; endif; ?>
+
+						</div>
+
+				</div>
+
+			</div>
+
+<?php get_footer(); ?>
