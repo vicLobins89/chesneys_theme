a4344ed340f27638ce0673e6738ab3daf644ddf2
vicLobins89 <vic.lobins@gmail.com>
2019-07-24 13:04:31 +0100

    js

--- functions.php
+++ functions.php
@@ -305,7 +305,7 @@ if ($userInfo->country->isoCode == 'US') {
 // Limited Edition
 function custom_catalog_ordering_args( $args ) {
 
-    if( !is_product_category('sir-john-soane', 'sir-edwin-lutyens') ) {
+    if( !is_product_category('sir-edwin-lutyens', 'sir-john-soane') ) {
         return $args;
     }
 
