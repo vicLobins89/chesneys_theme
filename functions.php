f095551a1c82471c4bfa8e241632ffced75a4f22
vicLobins89 <vic.lobins@gmail.com>
2019-07-25 08:07:11 +0100

    CURL

--- functions.php
+++ functions.php
@@ -55,6 +55,33 @@ function rarehoney_init() {
 // let's get this party started
 add_action( 'after_setup_theme', 'rarehoney_init' );
 
+
+
+/************* CURL *************/
+// Force certificate validation. You need a valid certificate in the site, self generated certificates are NOT VALID. 
+add_filter( 'https_local_ssl_verify', '__return_true' );
+
+// Setting a custom timeout value for cURL. Using a high value for priority to ensure the function runs after any other added to the same action hook.
+add_action('http_api_curl', 'sar_custom_curl_timeout', 9999, 1);
+function sar_custom_curl_timeout( $handle ){
+	curl_setopt( $handle, CURLOPT_CONNECTTIMEOUT, 30 ); // 30 seconds. Too much for production, only for testing.
+	curl_setopt( $handle, CURLOPT_TIMEOUT, 30 ); // 30 seconds. Too much for production, only for testing.
+}
+
+// Setting custom timeout for the HTTP request
+add_filter( 'http_request_timeout', 'sar_custom_http_request_timeout', 9999 );
+function sar_custom_http_request_timeout( $timeout_value ) {
+	return 30; // 30 seconds. Too much for production, only for testing.
+}
+
+// Setting custom timeout in HTTP request args
+add_filter('http_request_args', 'sar_custom_http_request_args', 9999, 1);
+function sar_custom_http_request_args( $r ){
+	$r['timeout'] = 30; // 30 seconds. Too much for production, only for testing.
+	return $r;
+}
+
+
 /************* OEMBED SIZE OPTIONS *************/
 
 if ( ! isset( $content_width ) ) {
